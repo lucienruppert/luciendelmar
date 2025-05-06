b41d6f394102ec0f5fa49ff38f6450c51604b7a7
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-05-06 15:58:23 +0200

    remove events component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { MethodComponent } from './components/method/method.component';
 import { VideosComponent } from './components/videos/videos.component';
-import { EventsComponent } from './components/events/events.component';
 import { VisionComponent } from './components/vision/vision.component';
 import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
@@ -31,11 +30,6 @@ export const routes: Routes = [
   {
     path: 'videos',
     component: VideosComponent,
-    data: { prerender: true }
-  },
-  {
-    path: 'events',
-    component: EventsComponent,
     data: { prerender: true }
   },
   {
