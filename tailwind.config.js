8a1c5d84691b1eb85e6b97bb2b49523a11f68dfd
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-05-07 15:50:24 +0200

    restyle

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: "#a18e58",
+      },
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+      },
+      screens: {
+        xs: { max: "500px" },
+        "2xs": { max: "400px" },
+      },
+    },
   },
   plugins: [],
 };
