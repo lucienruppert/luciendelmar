aa1183e52e3d2eb78119e896873e792e761f7ced
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-05-13 18:16:53 +0200

    add relief-therapy

---
+++ src/app/components/relief-therapy/relief-therapy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relief-therapy',
+  standalone: true,
+  imports: [],
+  templateUrl: './relief-therapy.component.html',
+  styleUrl: './relief-therapy.component.css'
+})
+export class ReliefTherapyComponent {
+
+}
