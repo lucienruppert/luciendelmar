9fcd1febf56de36073a7d84a97902ae93b4652d6
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-05-26 11:25:19 +0200

    add button

--- src/app/components/holding-conversation/holding-conversation.component.ts
+++ src/app/components/holding-conversation/holding-conversation.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-holding-conversation',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './holding-conversation.component.html',
-  styleUrls: ['./holding-conversation.component.css'],
+  styleUrls: ['./holding-conversation.component.css']
 })
 export class HoldingConversationComponent {
-  constructor() {}
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  public goToContactPage(): void {
+    this.router.navigate(['..', 'contact'], {
+      relativeTo: this.route,
+    });
+  }
 }
