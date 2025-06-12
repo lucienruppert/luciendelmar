37e0a4bea4d5b979e45215be75efd2a668cd6dc9
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-06-12 17:37:15 +0200

    restyle testimonials

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -8,11 +8,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './testimonials.component.html',
   styleUrls: ['./testimonials.component.css'],
 })
-export class TestimonialsComponent {
-  testimonials = [
-    {
-      text: 'A regressziós folyamat során olyan mély belátásokat nyertem, amelyek segítettek megérteni és elengedni régóta húzódó érzelmi mintáimat. Lucien biztonságos teret teremtett a munkához, és szakértő módon vezetett végig az úton.',
-      author: 'Anna K.',
-    },
-  ];
-}
+export class TestimonialsComponent {}
