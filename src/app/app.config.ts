318e1fc6b2dceacb0cf708f372a04338c07dd4cc
Ruppert Endre Lucien <121402304+lucienruppert@users.noreply.github.com>
2025-09-10 16:15:20 +0200

    fix top navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { importProvidersFrom } from '@angular/core';
@@ -9,7 +9,13 @@ import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      })
+    ),
     provideAnimationsAsync(),
     importProvidersFrom(YouTubePlayerModule, DialogModule),
     { provide: DIALOG_DATA, useValue: {} },
